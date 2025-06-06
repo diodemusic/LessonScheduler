f791465f4c74756776f696ce9996b8655dda11b1
diodemusic <diodemusicuk@gmail.com>
2025-06-06 18:34:17 +0100

    Add deleteModule controller

--- server/src/controllers/module.controller.js
+++ server/src/controllers/module.controller.js
@@ -8,7 +8,7 @@ async function getModules(req, res) {
     } catch (error) {
         res.status(400).send({ message: error.message });
     }
-}
+};
 
 async function postModule(req, res) {
     try {
@@ -20,9 +20,24 @@ async function postModule(req, res) {
     } catch (error) {
         res.status(400).end({ message: error.message });
     }
-}
+};
+
+async function deleteModule(req, res) {
+    try {
+        const module = await Module.findByIdAndDelete(req.params.id);
+
+        if (module == null) {
+            return res.status(404).send({ message: "Bad request" })
+        }
+
+        res.status(204).end();
+    } catch (error) {
+        res.status(400).end({ message: error.message });
+    }
+};
 
 module.exports = {
     getModules,
-    postModule
+    postModule,
+    deleteModule
 };
